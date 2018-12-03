1e033845a234b6a9478f0fbbe34fb398bde14d42
Rashed Abdel-Tawab <rashed@linux.com>
2018-12-03 19:55:06 +0530

    harpia: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: If1f47a91a3ffb6eb97a60e9c32d93e8326ec31ba

--- rr_harpia.mk
+++ rr_harpia.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
 # Inherit some common rr stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
